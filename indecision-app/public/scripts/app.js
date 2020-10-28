61a7b96e2f5e7d4ce9891ec621502b7ebc949899
Shaadr <mphanks@msn.com>
2020-10-28 09:03:13 -0600

    8. setup for react in html. JSX example and babeljs compile example.

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,1 +1,21 @@
 console.log('App.js is running!')
+
+// JSX - Javascript XML - JS Syntax extension
+var template = <p>This is JSX from app.js</p>;
+var appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot)
+
+/* babeljs.io will compile this code to browser readable code. The above compiles to: 
+
+"use strict";
+
+var template = React.createElement(
+    "p", 
+    null,
+     "This is JSX from app.js"
+     );
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
+
+*/
