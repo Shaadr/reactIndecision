aaec3aa11625146e63da378588f09b6ca25061ca
Shaadr <mphanks@msn.com>
2020-10-28 09:05:48 -0600

    8. working JSX example with babel-ized code -pre automation

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,21 +1,11 @@
-console.log('App.js is running!')
-
-// JSX - Javascript XML - JS Syntax extension
-var template = <p>This is JSX from app.js</p>;
-var appRoot = document.getElementById('app')
-
-ReactDOM.render(template, appRoot)
-
-/* babeljs.io will compile this code to browser readable code. The above compiles to: 
-
 "use strict";
 
+// JSX - Javascript XML - JS Syntax extension
 var template = React.createElement(
     "p", 
     null,
      "This is JSX from app.js"
      );
-var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+var appRoot = document.getElementById('app')
 
-*/
+ReactDOM.render(template, appRoot)
