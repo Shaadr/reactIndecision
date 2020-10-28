eace19e25e32c3101f703e8c1d2a3e690d859ee0
Shaadr <mphanks@msn.com>
2020-10-28 09:41:40 -0600

    setup babel compile and watch. moved yarn and package.json inside app folder.

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,16 +1,10 @@
-'use strict';
-
 /*This file is complied via babel command.
   from /indesicion-app run: 
   babel src/app.js --out-file=public/scripts/app.js --presets-env,react --watch
 */
 
 // JSX - Javascript XML - JS Syntax extension
-var template = React.createElement(
-  'p',
-  null,
-  'Testing babel compile and watch'
-);
-var appRoot = document.getElementById('app');
+var template = <p>Testing babel compile and watch</p>
+var appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template, appRoot)
