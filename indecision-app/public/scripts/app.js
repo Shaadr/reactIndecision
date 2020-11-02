b430214c3bb711861cc35dfeb22ee1d76c43afc3
Shaadr <mphanks@msn.com>
2020-11-01 17:56:06 -0700

    14. challenge: my solution

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 function square(x) {
     return x * x;
@@ -14,3 +14,20 @@ var squareArrow = function squareArrow(x) {
 };
 
 console.log(squareArrow(15));
+
+// Challenge - use arrow functions
+// getFirstName ('Mike Smith') -> 'Mike
+// Create regualr arrow function
+// Create using shorthand concise syntax
+
+var getFirstName = function getFirstName(fullName) {
+    return fullName.split(' ')[0];
+};
+
+var getFirstNameShorthand = function getFirstNameShorthand(fullName) {
+    return fullName.split(' ')[0];
+};
+
+console.log(getFirstName("Mike Smith"));
+
+console.log(getFirstNameShorthand("Jordan Hanks"));
--- indecision-app/src/playground/es6-arrowFunction.js
+++ indecision-app/src/playground/es6-arrowFunction.js
@@ -10,3 +10,18 @@ function square(x) {
 const squareArrow = x => x * x;
 
 console.log(squareArrow(15));
+
+// Challenge - use arrow functions
+// getFirstName ('Mike Smith') -> 'Mike
+// Create regualr arrow function
+// Create using shorthand concise syntax
+
+const getFirstName = (fullName) => {
+    return fullName.split(' ')[0]
+}
+
+const getFirstNameShorthand = fullName => fullName.split(' ')[0]
+
+console.log(getFirstName("Mike Smith"))
+
+console.log(getFirstNameShorthand("Jordan Hanks"))
