bd2305cf91c6f8f135849ab2dffe663a210ae28e
Shaadr <mphanks@msn.com>
2020-11-04 21:04:10 -0700

    18. create render function when options added challenge: my solution

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -17,26 +17,35 @@ const onFormSubmit = (e) => {
   if (option) {
     app.options.push(option);
     e.target.elements.option.value = '';
+    renderApp();
   }
 }
 
-// JSX - Javascript XML - JS Syntax extension
-const template = (
-  <div>
-    <h1>{app.title}</h1> 
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? "Here are your options:" : "No Options"}</p>
-    <p>{app.options.length}</p>
-    <ol>
-      <li>item one</li>
-      <li>item two</li>
-    </ol>
-    <form onSubmit={onFormSubmit}>
-      <input type="text" name="option"/>
-      <button>Add Option</button>
-    </form>
-  </div>
-);
+const renderApp = () => {
+  // JSX - Javascript XML - JS Syntax extension
+  const template = (
+    <div>
+      <h1>{app.title}</h1> 
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? "Here are your options:" : "No Options"}</p>
+      <p>{app.options.length}</p>
+      <ol>
+        <li>item one</li>
+        <li>item two</li>
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option"/>
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
 
-const appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+  const appRoot = document.getElementById('app');
+  ReactDOM.render(template, appRoot);
+}
+
+renderApp();
+//Challenge:
+//Create render fn that renders the new jsx
+//call it right away
+//call it after options array added to.
