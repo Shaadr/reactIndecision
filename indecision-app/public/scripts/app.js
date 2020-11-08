2c3a13ef167d1186803e9371ed68d3e50b62e752
Shaadr <mphanks@msn.com>
2020-11-07 21:59:35 -0700

    reset babel to show indesicion app. Section finish!

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,35 +1,95 @@
-"use strict";
+'use strict';
 
-var showing = false;
+/*This file is complied via babel command.
+  from /indesicion-app run: 
+  babel ./src/app.js --out-file=./public/scripts/app.js --presets=env,react --watch
+*/
 
-var onShowDetails = function onShowDetails() {
-    showing = !showing;
-    render();
+var app = {
+  title: 'Indesicion App',
+  subtitle: 'Making the hard decisions for you!',
+  options: []
 };
 
-var render = function render() {
-    var template = React.createElement(
-        "div",
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderApp();
+  }
+};
+
+var removeAllOptions = function removeAllOptions() {
+  app.options = [];
+  renderApp();
+};
+
+var onMakeDecision = function onMakeDecision() {
+  var randomNum = Math.floor(Math.random() * app.options.length);
+  var selectedOption = app.options[randomNum];
+  alert(selectedOption);
+};
+
+var renderApp = function renderApp() {
+  // JSX - Javascript XML - JS Syntax extension
+  var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      'p',
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length > 0 ? "Here are your options:" : "No Options"
+    ),
+    React.createElement(
+      'button',
+      { disabled: app.options.length === 0, onClick: onMakeDecision },
+      'What should I do?'
+    ),
+    React.createElement(
+      'button',
+      { onClick: removeAllOptions },
+      'Remove All'
+    ),
+    React.createElement(
+      'ol',
+      null,
+      app.options.map(function (option) {
+        return React.createElement(
+          'li',
+          { key: option },
+          'Option: ',
+          option
+        );
+      })
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
         null,
-        React.createElement(
-            "h1",
-            null,
-            "Visibility Toggle"
-        ),
-        React.createElement(
-            "button",
-            { onClick: onShowDetails },
-            showing ? "Hide Details" : "Show Details"
-        ),
-        showing && React.createElement(
-            "p",
-            null,
-            "More Details Shown!"
-        )
-    );
+        'Add Option'
+      )
+    )
+  );
 
-    var appRoot = document.getElementById('app');
-    ReactDOM.render(template, appRoot);
+  var appRoot = document.getElementById('app');
+  ReactDOM.render(template, appRoot);
 };
 
-render();
+renderApp();
