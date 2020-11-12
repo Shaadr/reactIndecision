97eaf66c1e75621d31f9d9406cb16b18ebeeedf2
Shaadr <mphanks@msn.com>
2020-11-11 20:51:58 -0700

    25. react-classes2.  Pre-challenge. rework if statment to one line.

--- indecision-app/src/playground/es6-classes1.js
+++ indecision-app/src/playground/es6-classes1.js
@@ -9,15 +9,37 @@ class Person {
     }
 
     getDescription(string) {
-        return `${this.name} is ${this.age} year(s) old!`
+        return `${this.name} is ${this.age} year(s) old.`
     }
 }
 
-const me = new Person('Mike Hanks', 30);
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription() {
+        let description = super.getDescription();
+
+        return this.hasMajor() ? description += ` Their major is ${this.major}.` : description
+
+        // if (this.hasMajor()) {
+        //     description += ` Their major is ${this.major}.` 
+        // }
+
+        // return description
+    }
+}
+
+const me = new Student('Mike Hanks', 30, 'Computer Science');
+console.log(me.hasMajor())
 console.log(me.getDescription())
 
-const other = new Person('Jordan Hanks', 34)
+const other = new Student('Jordan Hanks', 34)
+console.log(other.hasMajor())
 console.log(other.getDescription())
-
-const anonymous = new Person()
-console.log(anonymous.getDescription())
