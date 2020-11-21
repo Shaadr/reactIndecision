6744212aa4e45f3401d246a0f785d700163f815b
Shaadr <mphanks@msn.com>
2020-11-21 16:16:46 -0700

    41. setting up default options prop on IndecisionApp component example.

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -20,7 +20,7 @@ var IndecisionApp = function (_React$Component) {
     _this.handlePick = _this.handlePick.bind(_this);
     _this.handleAddOption = _this.handleAddOption.bind(_this);
     _this.state = {
-      options: []
+      options: props.options
     };
     return _this;
   }
@@ -82,6 +82,10 @@ var IndecisionApp = function (_React$Component) {
 
   return IndecisionApp;
 }(React.Component);
+
+IndecisionApp.defaultProps = {
+  options: []
+};
 
 var Header = function Header(props) {
   return React.createElement(
@@ -214,4 +218,4 @@ var AddOption = function (_React$Component2) {
 //     )
 // }
 
-ReactDOM.render(React.createElement(IndecisionApp, null), document.getElementById('app'));
+ReactDOM.render(React.createElement(IndecisionApp, { options: ['defaultOption1', 'defaultOption2'] }), document.getElementById('app'));
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -5,7 +5,7 @@ class IndecisionApp extends React.Component {
     this.handlePick = this.handlePick.bind(this);
     this.handleAddOption = this.handleAddOption.bind(this);
     this.state = {
-      options: []
+      options: props.options
     };
   };
 
@@ -67,6 +67,10 @@ class IndecisionApp extends React.Component {
       </div>
     );
   }
+}
+
+IndecisionApp.defaultProps = {
+  options: []
 }
 
 const Header = (props) => {
@@ -162,4 +166,4 @@ class AddOption extends React.Component {
 //     )
 // }
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp options={['defaultOption1','defaultOption2']}/>, document.getElementById('app'))
